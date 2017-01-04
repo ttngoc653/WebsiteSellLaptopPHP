82ed1b57acad82a9b5102482c84c7a1ca88f2cff
TranLeBaoLam-PC\TranLeBaoLam <doompi.doompi@gmail.com>
2017-01-04 16:46:19 +0700

    Thêm chức năng chi tiết sản phẩm

---
+++ Source/_ChitietSP.php
@@ -0,0 +1,339 @@
+<div class="container">
+	<div class="form-group">
+                      
+	<label for="Firstname" class="col-sm-4 control-label">Thông tin sản phẩm:</label>                     
+       			</div>
+    <div class="row">
+
+ 		  <div class="col-md-6">
+          		<img src="https://cdn.tgdd.vn/Products/Images/44/76031/acer-es1-431-n3060-4gb-500gb-win10-400x400.png" width="100px" />     
+          </div>
+          <div class="col-md-6">
+    	<form class="form-horizontal" action="index.html" method="POST">
+        <div class="form-group">
+          			<label for="DGSP" class="col-sm-6 control-label">định giá sản phẩm</label>
+                    
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<div class="form-group">
+							<label for="SoLuong" class="col-sm-4 control-label">Số lượng:</label>
+								<div class="col-sm-8">
+									<input type="text" class="form-control" id="SoLuong">
+								</div>
+							</div>       
+						</div>
+                    <div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<button type="submit" class="btn btn-success">
+								<i class="fa fa-check"></i>Thêm vào giỏ hàng                              
+							</button>
+						</div>
+					</div>
+                    
+                </div> 
+          </form>
+          </div>
+        
+	</div>
+
+  <h2>Thông số sản phẩm</h2>
+           
+  <table class="table">
+   <thead>
+   		
+      <tr>
+        <th>Bộ xử lý</th>
+        <th></th>
+      </tr>
+
+    </thead>
+    <tbody>
+      <tr>
+        <td>Hãng CPU</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Công nghệ CPU</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Loại CPU</td>
+        <td>...</td>
+      </tr>
+       <tr>
+        <td>Tốc độ CPU</td>
+        <td>...</td>
+      </tr>
+       <tr>
+        <td>Bộ nhớ đệm</td>
+        <td>...</td>
+      </tr>
+       <tr>
+        <td>Tốc độ tối đa</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+       <thead>
+      <tr>
+        <th>Bo mạch</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Chipset</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Tốc độ Bus</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Hỗ trợ RAM tối </td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+    
+    <thead>
+      <tr>
+        <th>Bộ nhớ</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>RAM</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Loại RAM</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Tốc độ Bus</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+  
+       <thead>
+      <tr>
+        <th>Đĩa cứng</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Loại ổ đĩa</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Ổ cứng</td>
+        <td>...</td>
+      </tr>
+
+       <thead>
+      <tr>
+        <th>Màn hình</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Kích thước màn hình</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Độ phân giải (W x H)</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Công nghệ màn hình</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Màn hình cảm ứng</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+
+ <thead>
+      <tr>
+        <th>Đồ họa</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Chipset đồ họa</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Bộ nhớ đồ họa</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Thiết kế card</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+ <thead>
+      <tr>
+        <th>Âm thanh</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Kênh âm thanh</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Thông tin thêm</td>
+        <td>...</td>
+      </tr>
+    </tbody>    
+
+ 	<thead>
+      <tr>
+        <th>Đĩa quang</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Có sẳn đĩa quang</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Loại đĩa quang</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+
+ <thead>
+      <tr>
+        <th>Tính năng mở rộng & Cổng giao tiếp</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Cổng giao tiếp</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Tính năng mở rộng</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+ <thead>
+      <tr>
+        <th>Giao tiếp mạng</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>LAN</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Chuẩn Wifi</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Kết nối không dây</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+ 	<thead>
+      <tr>
+        <th>Card Reader</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Đọc thẻ nhớ</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Khe đọc thẻ nhớ</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+ <thead>
+      <tr>
+        <th>Webcam</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Độ phân giải WC</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Thông tin thêm</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+ <thead>
+      <tr>
+        <th>PIN/Battery</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Thông tin Pin</td>
+        <td>...</td>
+      </tr>
+    </tbody> 
+    
+    
+ <thead>
+      <tr>
+        <th>Hệ điều hành, phần mềm sẵn có/OS</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Hệ điều hành</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Phần mềm có sẳn</td>
+        <td>...</td>
+      </tr>
+    </tbody>
+    
+    
+ <thead>
+      <tr>
+        <th>Kích thước & Trọng lượng</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Kích thước</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Trọng lượng (kg)</td>
+        <td>...</td>
+      </tr>
+      <tr>
+        <td>Chất liệu</td>
+        <td>...</td>
+      </tr>
+    </tbody>    
+</table>
+</div>
