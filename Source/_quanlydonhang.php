0934f15b5031d0e898c43d43da3c5c1cb1f4ceef
TranLeBaoLam-PC\TranLeBaoLam <doompi.doompi@gmail.com>
2017-01-10 17:44:52 +0700

    Thêm chức năng quản lý đơn hàng

---
+++ Source/_quanlydonhang.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- sremove-9caa494cf5dd5d66eb198271b9be17cc --><script></script><!-- sremove-end -->
+  <!-- sremove-ec54aaae5568947f433ab2c8d75fca91 --><script></script><!-- sremove-end -->
+</head>
+<body>
+
+<div class="container">
+  <h2>Table</h2>
+  <p>The .table-responsive class creates a responsive table which will scroll horizontally on small devices (under 768px). When viewing on anything larger than 768px wide, there is no difference:</p>                                                                                      
+  <div class="table-responsive">          
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Stt</th>
+        <th>Thông tin đơn hàng</th>
+        <th>Thông tin người mua</th>
+        <th>Tình trạng đơn hàng</th>
+        <th>Thao tác</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Anna</td>
+        <td>Pitt</td>
+        <td>New York</td>
+        <td>Xem chi tiết</td>
+      </tr>
+    </tbody>
+    <tbody>
+      <tr>
+        <td>2</td>
+        <td>Anna</td>
+        <td>Pitt</td>
+        <td>New York</td>
+        <td>Xem chi tiết</td>
+      </tr>
+    </tbody>
+    <tbody>
+      <tr>
+        <td>3</td>
+        <td>Anna</td>
+        <td>Pitt</td>
+        <td>New York</td>
+        <td>Xem chi tiết</td>
+      </tr>
+    </tbody>
+    <tbody>
+      <tr>
+        <td>4</td>
+        <td>Anna</td>
+        <td>Pitt</td>
+        <td>New York</td>
+        <td>Xem chi tiết</td>
+      </tr>
+    </tbody>
+    
+  </table>
+  </div>
+</div>
+
+</body>
+</html>
