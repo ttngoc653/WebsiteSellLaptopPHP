9ad7d63521e65d5bfc241a0722d1ae552a81069f
TranLeBaoLam-PC\TranLeBaoLam <doompi.doompi@gmail.com>
2017-01-11 22:24:44 +0700

    xóa chức năng không cần thiết file index.php

--- Source/index.php
+++ Source/index.php
@@ -32,9 +32,7 @@
           case "themsp":
             include_once "./_themSP.php";
             break;
-          case "themcpu":
-            include_once "./_themCPU.php";
-            break;
+
           case "themnsx":
             include_once "./_themHangSX.php";
             break;
