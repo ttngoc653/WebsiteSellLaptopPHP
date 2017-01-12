cd56c07a6735829564b1fb4cffff255707d6a90c
C20161009\Hioh <coldboy6563@gmail.com>
2017-01-12 11:35:58 +0700

    sửa chân trang

--- Source/_chanWeb.php
+++ Source/_chanWeb.php
@@ -2,7 +2,7 @@
 <div class="panel-body">
 	<?php 
 		$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; 
-		if(isset($role)) echo "<a href=\"".$_SERVER["REQUEST_URI"]."&gio="."\"><button style=\"height: 20px; background-color: #E6E6E6;color: blue;border-radius: 5px;\">Thêm vào giỏ</button></a>";
+		if(isset($role)) "<a href=\"".$_SERVER["REQUEST_URI"]."&gio="."\"><button style=\"height: 20px; background-color: #E6E6E6;color: blue;border-radius: 5px;\">Thêm vào giỏ</button></a>";
 	?>
 	MÔN LẬP TRÌNH WEB 1 - ĐỒ ÁN CUỐI MÔN HỌC CUỐI KỲ<br/>
 	SINH VIÊN THỰC HIỆN: 1460510 - TRẦN LÊ BẢO LÂM & 1460653 - TRẦN THẾ NGỌC
--- Source/_dauWeb.php
+++ Source/_dauWeb.php
@@ -16,9 +16,9 @@
 		<a class="" href="index.php"><img src="./image/home.png" width="45px" alt=""/></a>
 	</div>
 	<div  style="margin-top: -42px;margin-left: 182px; margin-right: 500px; text-align: right; margin-right: 0px;position: absolute; color: white;font-weight: bold;font-size: 12px;">
-		<?php include_once "./_timTheoDieuKien.php" ?>
+		<?php include_once "./_timTheoDieuKien.php"; ?>
 	</div>
-	<div style="margin-top: -52px;margin-left: 624px; text-align: right; margin-right: 0px;position: absolute; color: white;font-weight: bold;font-size: 12px;">
+	<div style="margin-top: -52px;margin-left: 624px; text-align: right; position: absolute; color: white;font-weight: bold;font-size: 12px;">
 		<?php include_once "./_dangNhap.php"; ?>
 	</div>
 	<?php 
